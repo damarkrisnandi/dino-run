05ffbbbbb47058677696d0c7018bb248ad6ce7fe
damar <damar@gitlab.lab>
2020-01-03 18:59:31 +0700

    update gameplay

--- Assets/Player1Movement.cs
+++ Assets/Player1Movement.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class Player1Movement : MonoBehaviour
 {
     public Rigidbody rb;
+    public bool isGrounded;
+    public Vector3 startingPoint;
     // Update is called once per frame
+
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        startingPoint = transform.position;
+    }
+
+    void OnCollisionStay(Collision coll)
+    {
+        if (coll.collider.name == "Arena") {
+            isGrounded = true;
+        } else {
+            Debug.Log("Nabrak Kaktus");
+            transform.position = startingPoint;
+        }
+        
+    }
+
     void Update()
     {
-        rb.AddForce(1000 * Time.deltaTime, 0 , 0);
+        rb.AddForce(10, 0 , 0);
 
         if (Input.GetKey("w")) {
             rb.AddForce(0, 0, 20);
@@ -18,8 +37,10 @@ public class Player1Movement : MonoBehaviour
             rb.AddForce(0, 0, -20);
         }
 
-        if (Input.GetKey("space")) {
-            rb.AddForce(0, 90 - 10 * Time.deltaTime, 0);
+        if (Input.GetKey("space") & isGrounded == true) {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            isGrounded = false;
         }
+
     }
 }
