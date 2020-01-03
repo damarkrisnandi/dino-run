dc83531bfd7c48eb7ed93364d668e3c1ad0a457a
damar <damar@gitlab.lab>
2020-01-04 00:27:42 +0700

    constant velocity

--- Assets/Player1Movement.cs
+++ Assets/Player1Movement.cs
@@ -7,11 +7,13 @@ public class Player1Movement : MonoBehaviour
     public Rigidbody rb;
     public bool isGrounded;
     public Vector3 startingPoint;
+    public bool isAccel;
     // Update is called once per frame
 
     void Start(){
         rb = GetComponent<Rigidbody>();
         startingPoint = transform.position;
+        isAccel = false;
     }
 
     void OnCollisionStay(Collision coll)
@@ -25,12 +27,23 @@ public class Player1Movement : MonoBehaviour
         
     }
 
+    public float constantSpeed;
+    public float smoothingFactor  = 1.0f;
+ 
+    void getVelocity()
+    {
+        rb.velocity = constantSpeed * (rb.velocity.normalized);
+    }
+
     void FixedUpdate()
     {
+        if (isAccel == false) {
+            getVelocity();
+        }
         // rb.AddForce(new Vector3(5, 0 , 0));
 
         if (Input.GetKey("space") & isGrounded == true) {
-            rb.AddForce(new Vector3(-1, 5, 0), ForceMode.Impulse);
+            rb.AddForce(new Vector3(0, constantSpeed, 0), ForceMode.Impulse);
             isGrounded = false;
         }
     }
