cb06db625e41a25847463f86990a6db7cb5fe8fb
damar <damar@gitlab.lab>
2020-01-03 14:26:33 +0700

    cek

---
+++ Assets/Player1Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d")) {
+            rb.AddForce(20, 0 , 0);
+        }
+
+        if (Input.GetKey("w")) {
+            rb.AddForce(0, 0, 20);
+        }
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-20, 0, 0);
+        }
+
+        if (Input.GetKey("s")) {
+            rb.AddForce(0, 0, -20);
+        }
+
+        if (Input.GetKey("space")) {
+            rb.AddForce(0, 25, 0);
+        }
+    }
+}
