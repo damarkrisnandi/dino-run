e86fcb0cbc5e673279e49bc6ce568b9c59fea7d7
damar <damar@gitlab.lab>
2020-01-07 07:23:56 +0700

    fix movement, create mountain

--- Assets/Player1Movement.cs
+++ Assets/Player1Movement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using static System.Random;
 using System;
 
+// http://localhost:62854/
 public class Player1Movement : MonoBehaviour
 {
     public Rigidbody rb;
@@ -18,6 +19,7 @@ public class Player1Movement : MonoBehaviour
     public GameObject cactees3;
     public GameObject cactees2;
     public GameObject cactees1;
+    public GameObject mountain;
 
     public float score;
     public TextMesh text;
@@ -29,6 +31,7 @@ public class Player1Movement : MonoBehaviour
         cf = GetComponent<ConstantForce>();
         rb = GetComponent<Rigidbody>();
         GenerateObstacle();
+        GenerateMountain();
 
         startingPoint = transform.position;
         isAccel = false;
@@ -41,17 +44,17 @@ public class Player1Movement : MonoBehaviour
     int r = 50;
     int type;
     void GenerateObstacle() {
-        for (int x = 1; x <= 100; x++) {
+        for (int x = 1; x <= 200; x++) {
             random = new System.Random();
             r += random.Next(20,25);
             if (x <= 10) {
                 r = r - 0;
             } else if (x <= 25) {
-                r -= 8;
+                r -= 6;
             } else if (x <= 50) {
-                r -= 12;
+                r -= 8;
             } else {
-                r -= 15;
+                r -= 10;
             }
             
             type = random.Next(1,3);
@@ -72,6 +75,19 @@ public class Player1Movement : MonoBehaviour
         }
     }
 
+    int m = 0;
+    void GenerateMountain() {
+        Instantiate(mountain, new Vector3(0, -2.1f, 30), Quaternion.identity);
+        for (int x = 1; x <= 30; x++) {
+            random = new System.Random();
+            m += random.Next(20,25);
+            
+            type = random.Next(1,3);
+            Instantiate(mountain, new Vector3(10*m, -2.1f, 30*type), Quaternion.identity);
+                    
+        }
+    }
+
     void OnCollisionStay(Collision coll)
     {
         if (coll.collider.name == "Arena") {
@@ -88,6 +104,7 @@ public class Player1Movement : MonoBehaviour
     void getVelocity()
     {
         cf.force = new Vector3(5, 0, 0);
+        // rb.velocity = new Vector3(10, 0, 0);
     }
 
     void FixedUpdate()
@@ -106,7 +123,7 @@ public class Player1Movement : MonoBehaviour
             if (Input.GetKey(KeyCode.Return)) {
                 isPlayOn = true;
                 transform.position = startingPoint;
-                message.text = "";
+                message.text = "Press [SPACEBAR] to jump over obstacles";
             }
         } else {
             if (Input.GetKey(KeyCode.Return)) {
@@ -122,11 +139,20 @@ public class Player1Movement : MonoBehaviour
             if (isAccel == false) {
                 getVelocity();
             }
+
+            if (transform.position.x - startingPoint.x >= 1000) {
+                rb.AddForce(1, 0, 0);
+            }
             // rb.AddForce(new Vector3(5, 0 , 0));
 
             if (Input.GetKey("space") & isGrounded == true) {
-                rb.AddForce(new Vector3(-3, 5, 0), ForceMode.Impulse);
+                rb.AddForce(new Vector3(0, 7f, 0), ForceMode.Impulse);
+                // rb.velocity = new Vector3(0, 1, 0) * 5f;
                 isGrounded = false;
+            }
+
+            if (transform.position.y >= 5.7f) {
+                rb.AddForce(new Vector3(0, -1, 0), ForceMode.Impulse);
             }
         }
     }
@@ -147,7 +173,7 @@ public class Player1Movement : MonoBehaviour
 
     void UpdateScore() {
         if (isPlayOn == true) {
-            score = (transform.position.x - startingPoint.x);
+            score = (transform.position.x - startingPoint.x) * 0.1f;
             text.text = Math.Floor(score).ToString();
         }
 
